d36b558864326b2339627392d7b6d071ae1269d7
Pedro Soares <psoares@avenuecode.com>
2016-03-31 13:36:15 -0300

    finalizing

--- exercicio.py
+++ exercicio.py
@@ -5,7 +5,6 @@ import glob
 text_files = glob.glob('source_files/*')
 
 def file_contents(file_name):
-    print file_name
     f = open(file_name)
     try:
         return f.read()
@@ -16,6 +15,7 @@ source = dict((file_name, file_contents(file_name))for file_name in text_files)
 
 
 def map_function(k, v):
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
     from stopwords import allStopWords
     for line in v.splitlines():
         authors_list = line.split(":::")[1]
@@ -23,31 +23,40 @@ def map_function(k, v):
 	for author in authors_list.split("::"):
 	        for word in terms.split():
                         word = word.lower()
-			if(word not in allStopWords):
-				yield author,{word: 1}
+			no_punctuation_word = ''
+			for char in word:
+				if char not in punctuations:
+					no_punctuation_word = no_punctuation_word + char
+			if(no_punctuation_word not in allStopWords):
+				yield author,{no_punctuation_word: 1}
             
 
 def reduce_function(k, v):
-    print "value: " + k
+    print "Reducing for Author: " + k
     dictionary = {}
     for index, item in enumerate(v):
 	for key,value in item.iteritems():
         	value_old = dictionary.get(key,0)
 		dictionary[key] = value_old + value 
 	
-    return dictionary
+    return sorted(dictionary.iteritems(), key=lambda (k,v): (v,k),reverse=True)
 
 
 s = mincemeat.Server()
-
-# The data source can be any dictionary-like object
 s.datasource = source
 s.mapfn = map_function
 s.reducefn = reduce_function
 
 results = s.run_server(password="changeme")
 
-print results
+print "As duas palavras que mais aparecem para Grzegorz Rozenberg  sao:"
+print results['Grzegorz Rozenberg'][0][0]
+print results['Grzegorz Rozenberg'][1][0]
+
+print "As duas palavras que mais aparecem para Philip S. Yu sao:"
+print results['Philip S. Yu'][0][0]
+print results['Philip S. Yu'][1][0]
+
 
 w = csv.writer(open("results.csv", "w"))
 for k, v in results.items():
